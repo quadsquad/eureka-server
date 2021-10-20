9ec95ef3c8da4c55b2be33270c5e4d35f83c3438
quadsquad <quadsquad1997@gmail.com>
2021-10-20 13:16:34 +0100

    add comment to EurekaServerApplication.java

--- src/main/java/com/eureka/server/EurekaServerApplication.java
+++ src/main/java/com/eureka/server/EurekaServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
-
+	//Main method to run eureka server
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
